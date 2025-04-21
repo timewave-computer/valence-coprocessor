de94d4b561012d77b6decd9bc1de80ec69abe92f
Victor Lopes <vhrlopes@gmail.com>
2025-04-21 15:05:32 +0200

    fix(smt): allow value replace (#13)
    * feat(smt): add rocksdb support (#9) (#10)
    
    * fix(smt): allow value replace
    
    This commit fixes the SMT behavior that will iterate over the bits of
    the key until depletion. It instead replaces the value upon depletion.
    
    It assumes the hash cannot produce more bits than [HASH_LEN] so a
    collision is necessarily the same data being inserted.
    
    related to #12
    
    * improve docs

--- crates/smt/src/tests.rs
+++ crates/smt/src/tests.rs
@@ -4,6 +4,41 @@ use valence_coprocessor_core::ExecutionContext;
 
 use crate::{Smt, TreeBackend};
 
+#[test]
+#[cfg(feature = "memory")]
+fn value_replace_single() {
+    let context = "value";
+    let mut smt = crate::MemorySmt::default();
+    let root = crate::MemorySmt::empty_tree_root();
+
+    let v = b"Nothing in life is to be feared, it is only to be understood. ";
+
+    for _ in 0..10 {
+        let root = smt.insert(root, context, v.to_vec()).unwrap();
+
+        assert!(smt.leaf_exists(context, root, v).unwrap());
+    }
+}
+
+#[test]
+#[cfg(feature = "memory")]
+fn value_replace_multiple() {
+    let context = "value";
+    let mut smt = crate::MemorySmt::default();
+    let root = crate::MemorySmt::empty_tree_root();
+
+    let x = b"The most beautiful thing we can experience is the mysterious.";
+    let v = b"Nothing in life is to be feared, it is only to be understood. ";
+
+    let root = smt.insert(root, context, x.to_vec()).unwrap();
+
+    for _ in 0..10 {
+        let root = smt.insert(root, context, v.to_vec()).unwrap();
+
+        assert!(smt.leaf_exists(context, root, v).unwrap());
+    }
+}
+
 fn property_check<B, C>(mut tree: Smt<B, C>, numbers: Vec<u32>)
 where
     B: TreeBackend,
